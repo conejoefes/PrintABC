f88c16299293adab2dd686020412bb377613bf80
conejoefes <47231967+conejoefes@users.noreply.github.com>
2021-03-24 20:13:18 -0600

    Update Main.java
    Comments added

--- src/com/indiecode/Main.java
+++ src/com/indiecode/Main.java
@@ -5,14 +5,15 @@ import com.indiecode.function.ShowABC;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Create instace of ShowABC Class
         ShowABC a = new ShowABC();
         //Starts in 0 = A
         a.printABC();
         a.printABC(5);
         a.printABC(0,14);
 
-        //Starts in 1=A
+        //Starts in 1=A, 2=B, 3=C, and so on
+   
         a.printLetter(5);
     }
 
